1dc32e3483d553441f7b024a57c59649d3824fda
Leon Etienne (ubuntu wsl) <leonetienne@hotmail.de>
2021-01-23 21:44:00 +0100

    Fixed favicons -.-

--- Tubio/frontend/_nuxt/static/1611433648/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1611433648",layout:"default",error:null,state:{diskUsage:{usage:{}},dlcache:{cache:[]},logs:{logs:[]},serverOs:{os_name:""},settings:{config:{}}},serverRendered:true,routePath:"\u002F",config:{}};
