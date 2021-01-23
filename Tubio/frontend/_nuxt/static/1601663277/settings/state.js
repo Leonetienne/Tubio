6161c48130dc600a9a0d998a4cecca1c5176cec5
Leon Etienne (ubuntu wsl) <leonetienne@hotmail.de>
2021-01-23 21:24:09 +0100

    Built

--- Tubio/frontend/_nuxt/static/1601663277/settings/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1601663277",layout:"default",error:null,state:{diskUsage:{usage:{}},dlcache:{cache:[]},logs:{logs:[]},serverOs:{os_name:""},settings:{config:{}}},serverRendered:true,routePath:"\u002Fsettings",config:{}};
